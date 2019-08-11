84def94af2f2cbd6b1702d1ac33530b4ead0013e
Finn Thye <piqueen@gmail.com>
2019-08-11 00:08:31 +0000

    Complete #consolidate_cart

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,22 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  total_hash = {}
+  
+  cart.each do |item|
+    if total_hash[item.keys[0]]
+      #increase value of :count key by 1
+      total_hash[item.keys[0]][:count] += 1
+    else
+      #add key/value pair, initialized :count = 0
+      total_hash[item.keys[0]] = {}
+      total_hash[item.keys[0]][:count] = 1
+      total_hash[item.keys[0]][:price] = item.values[0][:price]
+      total_hash[item.keys[0]][:clearance] = item.values[0][:clearance]
+    end
+  end
+  
+  total_hash
 end
 
 def apply_coupons(cart, coupons)
